7c13ffef3f7f5eff0f4ff022eed797051d66db19
Loxphordex <silasishallahan@gmail.com>
2019-05-08 13:30:28 -0700

    ACAO

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -25,7 +25,8 @@ const ApiService = {
     return fetch(`${config.API_ENDPOINT}/auth/login`, {
       method: 'POST',
       headers: {
-        'content-type': 'application/JSON'
+        'content-type': 'application/JSON',
+        'Access-Control-Allow-Origin': 'https://loxphordex-quoter-client.now.sh/login'
       },
       body: JSON.stringify({ username, password })
     })
